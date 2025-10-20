585f4a33ae61106c3c96e35a803b4c1cef62c251
PilgrimV1 <erkh_munkh@yahoo.com>
2025-10-20 07:44:37 +0200

    Completed Section: Reading Single Molecules

--- README.md
+++ README.md
@@ -2,10 +2,22 @@
 
 Made for my Internship at **IPB Halle** as a **Computational Chemist**
 
-## Topics I will be learning:
-- Python
-- RDKit Library
-- Machine Learning
-- Linux
-- React
-- Java
+---
+
+## 📚 Topics I will be learning
+
+- Python  
+- RDKit Library  
+- Machine Learning  
+- Linux  
+- React  
+- Java  
+
+---
+
+## 📝 Notes
+
+This journal will track my progress and experiments in RDKit, Python, and related tools.
+
+
+# Test Header
--- python-practice/1_Reading_Drawing_Writing/Reading_Single_Molecules.py
+++ python-practice/1_Reading_Drawing_Writing/Reading_Single_Molecules.py
@@ -7,16 +7,25 @@ You must activate the conda environment via the command on linux:
 conda activate rdkit-env
 """
 
-from rdkit import Chem          #loads important functions, "Chem" is the core library working with Python
-from rdkit.Chem import Draw     #rdkit.Chem
+from rdkit import Chem          # loads important functions, "Chem" is the core library working with Python
+from rdkit.Chem import Draw     # rdkit.Chem
 
 # Construct Molecule from SMILES Formula directly
 m = Chem.MolFromSmiles('Cc1ccccc1')
 
-# Draw the Image
-img = Draw.MolToImage(m)
+img = Draw.MolToImage(m) # Draw the Image
 img.show()      # To Show the Image
 
-m2 = Chem.MolFromSmiles('c1cc1') #Will display "Can't kekulize mol" as the formula is incorrect. Also shows the Unkekulized atoms
+m1 = Chem.MolFromSmiles('CO(C)C') # Explicit valence for atom # 1 0, 3 is greater permitted (SMILES Formula does not exist)
 
+m2 = Chem.MolFromSmiles('c1cc1')  # Will display "Can't kekulize mol" as the formula is incorrect. Also shows the Unkekulized atoms
 
+# In each case of m1 and m2 the value "None" is returned 
+
+#m1 is None 
+print(m1 is None) # Outputs "True" because the value of "None" was returned
+
+#m2 is None 
+print(m2 is None) # Outputs "True" because the value of "None" was returned as well
+
+print(m is None) # Will output "False" because m had a viable SMILES formula
