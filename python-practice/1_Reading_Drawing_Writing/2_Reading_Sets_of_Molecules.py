43a596f9b59eff614b2922e0bddf5f240b8fb079
PilgrimV1 <erkh_munkh@yahoo.com>
2025-10-20 10:47:46 +0200

    Added to Section 1.2:Reading Sets of Molecules

--- python-practice/1_Reading_Drawing_Writing/2_Reading_Sets_of_Molecules.py
+++ python-practice/1_Reading_Drawing_Writing/2_Reading_Sets_of_Molecules.py
@@ -1,6 +1,9 @@
-from rdkit import Chem
-from rdkit import Draw
+from rdkit import Chem 
 
-suppl = Chem.SDMolSupplier('')
-for mol in suppl:
-    print(mol.GetNumAtoms)
+
+# The .sdf file is not a given but you have to extract it from another repo
+suppl = Chem.SDMolSupplier('Sdf_files/chebi_test.sdf')   # suppl being the variable you can name... in reality it can be anything you name it 
+for mol in suppl:                              # mol standing for the molecules in the suppl variable.. variable within the overall 
+    print(mol.GetNumAtoms())                     # GetNumAtoms() function shows the number of Atoms within the Chemical Compound.. the print() function prints it on the terminal
+
+    
