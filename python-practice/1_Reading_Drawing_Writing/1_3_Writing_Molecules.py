29c97fe7a260b8ee2b318302ab6358e1437c6f5c
PilgrimV1 <erkh_munkh@yahoo.com>
2025-10-23 16:18:07 +0200

    update to section 1.3

--- python-practice/1_Reading_Drawing_Writing/1_3_Writing_Molecules.py
+++ python-practice/1_Reading_Drawing_Writing/1_3_Writing_Molecules.py
@@ -1,6 +1,13 @@
 from rdkit import Chem
 
-m = Chem.MolFromMolFile('molfiles/MolView.mol') # get a mol file from MolView.org website
+m = Chem.MolFromMolFile('molfiles/Pyridine.mol') # get a mol file from MolView.org website
 print(Chem.MolToSmiles(m))
 
-Chem.MolToSmiles(m,isomericSmiles=False)        # removing the isomericSmiles
+print(Chem.MolToSmiles(m,isomericSmiles=False))
+
+
+# removing the isomericSmiles... thought the SMILES provided is canonical so it should print out the same result no matter how a 
+# particular molecule is input
+print(Chem.MolToSmiles(Chem.MolFromSmiles('C1=CC=CN=C1')))
+print(Chem.MolToSmiles(Chem.MolFromSmiles('c1cccnc1')))
+print(Chem.MolToSmiles(Chem.MolFromSmiles('n1ccccc1')))
