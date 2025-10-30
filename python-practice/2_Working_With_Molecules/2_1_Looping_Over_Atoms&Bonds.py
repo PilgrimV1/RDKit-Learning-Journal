a515dfe389d749f63376660504537cbecf9e3bd9
PilgrimV1 <erkh_munkh@yahoo.com>
2025-10-30 15:37:48 +0100

    Completion of Chapter 2.1: Looping over Atoms and Bonds

--- python-practice/2_Working_With_Molecules/2_1_Looping_Over_Atoms&Bonds.py
+++ python-practice/2_Working_With_Molecules/2_1_Looping_Over_Atoms&Bonds.py
@@ -1,4 +1,5 @@
 from rdkit import Chem
+from io import StringIO
 
 m = Chem.MolFromSmiles('C1OC1')
 for atom in m.GetAtoms():
@@ -6,3 +7,21 @@ for atom in m.GetAtoms():
 
 
 print(m.GetBonds()[0].GetBondType())
+
+# Requesting Individual Atoms or Bonds
+
+print(m.GetAtomWithIdx(0).GetSymbol())
+
+print(m.GetAtomWithIdx(0).GetExplicitValence())
+
+print(m.GetBondBetweenAtoms(0,1).GetBondType())
+
+
+
+
+# Atoms to keep track of their neighbors
+
+atom = m.GetAtomWithIdx(0)
+[x.GetAtomicNum() for x in atom.GetNeighbors()]
+
+len(atom.GetNeighbors()[-1].GetBonds())
